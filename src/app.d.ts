915725a3e5d28df9e34b33192390afa1283aee40
idotter <marc@idotter.ch>
2025-10-21 17:09:29 +0200

    Define App namespace with Locals interface
    Add type definitions for App namespace including Locals interface.

---
+++ src/app.d.ts
@@ -0,0 +1,15 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		interface Locals {
+			session: import('@supabase/supabase-js').Session | null;
+			user: import('@supabase/supabase-js').User | null;
+		}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+export {};
