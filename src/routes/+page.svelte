5435c63c14dfc1e5e77addb9cc06e021af7f3740
idotter <marc@idotter.ch>
2025-10-21 16:17:26 +0200

    Add automatic redirect to registration page
    Redirects to the registration page on mount.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        goto('/register');
+    });
+</script>
+
+<div class="min-h-screen flex items-center justify-center bg-gray-50">
+    <p class="text-gray-600">Lade...</p>
+</div>
