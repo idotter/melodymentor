2b8ff94c39ff9c6acb6abfb689791784d3a3e701
idotter <marc@idotter.ch>
2025-10-22 14:54:01 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'], // Sucht in allen Svelte/JS/TS/HTML-Dateien
+  theme: {
+    extend: {}, // Hier könnten wir später eigene Farben/Schriften hinzufügen
+  },
+  plugins: [],
+}
